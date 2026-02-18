84d894edf8e1f2095e17e21b1331aa3d12add0d1
BajiLinux <mr.baji.smart@gmail.com>
2026-02-18 11:25:15 +0700

    done

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 // Menggunakan model stabil "gemini-1.5-flash" (Cepat & Murah)
 const model = genAI.getGenerativeModel({ 
-    model: "gemini-flash-lite-latest",
+    model: "gemini-2.0-flash",
     generationConfig: {
         temperature: 0.3,       
         maxOutputTokens: 800,   
